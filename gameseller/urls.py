7d102ca56f8f50c84c2c97a3d5b470e02a210c3a
ybgirgin3 <ybgirgin3@gmail.com>
2021-08-03 13:29:09 +0300

    register ekle

--- gameseller/urls.py
+++ gameseller/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('product.urls'))
 ]
--- register/models.py
+++ register/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Register(models.Model):
+    pass
     
