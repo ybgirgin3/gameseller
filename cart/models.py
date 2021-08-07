5ee36e1451c43f291f5a1517ec8e2da8e535d5ea
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2021-08-07 13:20:25 +0300

    cart models added

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    item = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.PositiveBigIntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        #return super().__str__()
+        return "{} - {} - {} - {} - {}".format(self.user,
+                                               self.item,
+                                               self.quantity,
+                                               self.created_at,
+                                               self.updated_at)
+
