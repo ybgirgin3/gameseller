8837bd0d7d6ebdc78d8832c77ec1cd7b62f3e3bb
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2021-08-07 16:14:27 +0300

    register üzerinden yeniden geçiyorum

--- cart/models.py
+++ cart/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+
+# user'ı contrib üzerinden değil register app üzerinden alacağız
 from django.contrib.auth.models import User
 from product.models import Product
 
 # Create your models here.
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    item = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    item = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
     quantity = models.PositiveBigIntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now=True)
